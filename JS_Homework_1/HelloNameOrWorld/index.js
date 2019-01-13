d1b0e469eaa74ff8fd79e3266abd71d50daa2651
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-01-13 18:44:49 +0200

    Hello_Name_kommit

---
+++ JS_Homework_1/HelloNameOrWorld/index.js
@@ -0,0 +1,12 @@
+function hello(name){
+  if(name != ''){
+    console.log('Hello ' + name[0].toUpperCase() + name.substring(1).toLowerCase() +'!');
+  } else {
+    console.log('Hello World!');
+  }
+}
+
+let fullname;
+fullname = window.prompt("Enter your name, please");
+
+hello(fullname);
