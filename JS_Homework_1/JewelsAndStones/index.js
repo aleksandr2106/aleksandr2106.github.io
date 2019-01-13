00c271f4514cc9650bb1cb478d9b6a63d2777b05
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-01-13 17:49:36 +0200

    Starting_Homework=)

---
+++ JS_Homework_1/JewelsAndStones/index.js
@@ -0,0 +1,20 @@
+let jewels, stones;
+let mas_jewels;
+let mas_stones;
+let count = 0;
+
+jewels = window.prompt("Enter your jewels, please");
+stones = window.prompt("Enter your stones, please");
+
+mas_jewels = jewels.split('');
+mas_stones = stones.split('');
+
+for( let i = 0; i < mas_jewels.length; i++ ){
+  for (let j = 0; j < mas_stones.length; j++) {
+    if( mas_jewels[i] == mas_stones[j] ){
+      count++;
+    }
+  }
+}
+
+console.log('Count of our jewels = ' + count);
---
+++ Lesson_5/index.js
@@ -0,0 +1,46 @@
+//          First Task
+
+// var result = 0;
+// do {
+//   result = window.prompt("Enter number greater than 100, please");
+// } while ((result <= 100) && (result ==0) );
+//
+// console.log("Access");
+
+
+
+//          Second Task
+
+//
+// for (let i = 2; i <= 100; i++) {
+//   let prime = true;
+//
+//   for (let j = 2; j < i; j++) {
+//     if (i % j === 0) {
+//       prime = false;
+//       break;
+//     }
+//   }
+//
+//   if (prime) {
+//     console.log(i);
+//   }
+//
+//   prime = false;
+// }
+
+
+
+
+for(var i=1; i<=100; i++ ){
+  if((i % 3 ==0) && (i % 5==0)){
+    console.log('FizzBuzz');
+  } else if ((i % 3==0)) {
+    console.log('Buzz');
+  } else if ((i % 5==0)) {
+    console.log('Fizz');
+  }
+  else {
+    console.log(i);
+  }
+}
