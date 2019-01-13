e63bc25f67bcda6d00311f6f74a3f2170c246086
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-01-13 18:13:27 +0200

    Feast_Kommit

---
+++ JS_Homework_1/TheFeastOfManyBeasts/index.js
@@ -0,0 +1,18 @@
+function feast(beast, dish) {
+  if( (beast[0] == dish[0]) && (beast[beast.length - 1] == dish[dish.length - 1]) ){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+let beast1, dish1;
+beast1 = window.prompt("Enter your beast, please");
+dish1 = window.prompt("Enter your dish, please");
+
+
+if(feast(beast1, dish1)){
+  console.log('Access');
+} else {
+  console.log('ERROR');
+}
