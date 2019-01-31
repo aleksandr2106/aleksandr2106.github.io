222edb7dc7c99949ac90cc267c0fd447424c9f52
aleksandr2106 <aleksandrkornienko19992106@gmail.com>
2019-01-31 20:48:13 +0200

    Fixed bug

--- JS_Form_with_validation/index.js
+++ JS_Form_with_validation/index.js
@@ -16,15 +16,18 @@ function showError(container, errorMessage) {
 
    function validate(form) {
      var elems = form.elements;
-
+     var counter = form.elements.length;
+     console.log(counter);
      resetError(elems.firstName.parentNode);
      if (!elems.firstName.value) {
        showError(elems.firstName.parentNode, 'Enter your First Name!!!');
+       counter--;
      }
 
     resetError(elems.lastName.parentNode);
      if (!elems.lastName.value) {
        showError(elems.lastName.parentNode, 'Enter your Last Name!!!');
+       counter--;
      }
      /*console.log(elems.gender.value);
      //resetError(elems.gender.parentNode);
@@ -36,25 +39,34 @@ function showError(container, errorMessage) {
      resetError(elems.password.parentNode);
      if (!elems.password.value) {
        showError(elems.password.parentNode, 'Enter password!!!');
+       counter-= 2;
      } else if (elems.password.value != elems.password2.value) {
        showError(elems.password.parentNode, 'Passwords do not match.');
+       counter-= 2;
      }
 
      resetError(elems.address.parentNode);
       if (!elems.address.value) {
         showError(elems.address.parentNode, 'Enter your address!!!');
+        counter--;
       }
 
       resetError(elems.email.parentNode);
        if (!elems.email.value) {
          showError(elems.email.parentNode, 'Enter your email!!!');
+         counter-= 2;
        } else if (elems.email.value.match(/@/)===null ) {
          showError(elems.email.parentNode, 'Email address is not valid!!!');
+         counter-= 2;
        }
 
      resetError(elems.country.parentNode);
      if (!elems.country.value) {
        showError(elems.country.parentNode, ' Select country!!!');
+       counter--;
      }
-
+     console.log(counter);
+     if(counter ===form.elements.length){
+       alert('Validation Passed');
+      }
    }
